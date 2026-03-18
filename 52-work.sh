6cef6a89bd865d396010eb99d8c43f88cda66b32
Evann <evann@grifon.fr>
2026-03-18 10:30:40 +0100

    feat(work): teams + pass

---
+++ 52-work.sh
@@ -0,0 +1,2 @@
+AddPackage --foreign teams-for-linux # Unofficial Microsoft Teams client for Linux using Electron.
+AddPackage pass # Stores, retrieves, generates, and synchronizes passwords securely
