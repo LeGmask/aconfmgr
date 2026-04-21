ce8d501a12019b49674b78bf36c26249a7c40431
Evann <evann@grifon.fr>
2026-04-21 22:54:41 +0200

    feat(profiles): add ship7

---
+++ profiles/ship7.sh
@@ -0,0 +1,5 @@
+ModuleLoad networks     networkmanager bluetooth utils
+ModuleLoad desktop      dotfiles hyprland
+ModuleLoad apps         entertainment keyboard mail tools
+ModuleLoad shell        bash zsh qol utils
+ModuleLoad laptop       power
