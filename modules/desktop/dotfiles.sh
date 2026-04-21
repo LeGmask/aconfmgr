f6f9dafa10743041b7bd2c4aad783c1ad203dd93
Evann <evann@grifon.fr>
2026-04-21 23:06:01 +0200

    feat(modules/desktop/dotfiles): auto apply

--- modules/desktop/dotfiles.sh
+++ modules/desktop/dotfiles.sh
@@ -1,1 +1,5 @@
 AddPackage chezmoi # Manage your dotfiles across multiple machines
+if command -v chezmoi >/dev/null 2>&1; then
+    [ -d "$HOME/.local/share/chezmoi" ] || chezmoi init LeGmask --ssh
+    chezmoi update
+fi
