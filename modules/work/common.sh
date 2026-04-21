abf67c0d3c33e71344305c768434bcc6cd38626a
Evann <evann@grifon.fr>
2026-04-21 22:34:53 +0200

    feat(modules): add work module

--- modules/work/common.sh
+++ modules/work/common.sh
@@ -1,2 +1,4 @@
 AddPackage pass # Stores, retrieves, generates, and synchronizes passwords securely
+AddPackage sshuttle # Transparent proxy server that forwards all TCP packets over ssh
+
 AddPackage --foreign clustershell # Python framework for efficient cluster administration
