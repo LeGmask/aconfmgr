d5009fcf66bbfa660db07b56d49c6f72d5e38869
Evann <evann@grifon.fr>
2026-04-21 23:32:33 +0200

    feat(modules/desktop/dotfiles): log dotfiles update

--- modules/desktop/dotfiles.sh
+++ modules/desktop/dotfiles.sh
@@ -1,5 +1,7 @@
 AddPackage chezmoi # Manage your dotfiles across multiple machines
 if command -v chezmoi >/dev/null 2>&1; then
+    LogEnter 'Applying dotfiles...\n'
     [ -d "$HOME/.local/share/chezmoi" ] || chezmoi init LeGmask --ssh
     chezmoi update
+    LogLeave
 fi
