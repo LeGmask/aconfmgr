1fe601abf58fda9f358be1e59b5e964e74e000b4
Evann DREUMONT <git@evann.dev>
2026-04-22 01:00:42 +0200

    fix(modules/desktop/dotfiles): apply after update

--- modules/desktop/dotfiles.sh
+++ modules/desktop/dotfiles.sh
@@ -2,6 +2,6 @@ AddPackage chezmoi # Manage your dotfiles across multiple machines
 if command -v chezmoi >/dev/null 2>&1 && [ "$aconfmgr_action" = "apply" ]; then
     LogEnter 'Applying dotfiles...\n'
     [ -d "$HOME/.local/share/chezmoi" ] || chezmoi init LeGmask --ssh
-    chezmoi update
+    chezmoi update --apply
     LogLeave
 fi
