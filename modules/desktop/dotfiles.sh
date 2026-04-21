907ea9c34363b0f6366dd26df6f4239c876726bb
Evann <evann@grifon.fr>
2026-04-21 23:35:29 +0200

    feat(modules/desktop/dotfiles): apply only when running aconfmgr apply

--- modules/desktop/dotfiles.sh
+++ modules/desktop/dotfiles.sh
@@ -1,5 +1,5 @@
 AddPackage chezmoi # Manage your dotfiles across multiple machines
-if command -v chezmoi >/dev/null 2>&1; then
+if command -v chezmoi >/dev/null 2>&1 && [ "$aconfmgr_action" = "apply" ]; then
     LogEnter 'Applying dotfiles...\n'
     [ -d "$HOME/.local/share/chezmoi" ] || chezmoi init LeGmask --ssh
     chezmoi update
