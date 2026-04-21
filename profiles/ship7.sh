477496798ed638adc922611c908af29f2a86f52c
Evann <evann@grifon.fr>
2026-04-22 00:07:59 +0200

    feat(profiles/ship7): add dev + enable sshd

--- profiles/ship7.sh
+++ profiles/ship7.sh
@@ -3,3 +3,7 @@ ModuleLoad desktop      dotfiles hyprland
 ModuleLoad apps         entertainment keyboard mail tools
 ModuleLoad shell        bash zsh qol utils
 ModuleLoad laptop       power
+
+ModuleLoad development
+
+SystemdEnable openssh /usr/lib/systemd/system/sshd.service
