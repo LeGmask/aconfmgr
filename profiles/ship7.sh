68bb138e25679f651c8c8344a72826ca96d11e96
Evann DREUMONT <git@evann.dev>
2026-04-22 23:45:42 +0200

    feat(profiles/ship7): enable desktop/niri

--- profiles/ship7.sh
+++ profiles/ship7.sh
@@ -1,5 +1,5 @@
 ModuleLoad networks     networkmanager bluetooth utils
-ModuleLoad desktop      dotfiles hyprland
+ModuleLoad desktop      dotfiles hyprland niri
 ModuleLoad apps         entertainment keyboard mail tools
 ModuleLoad shell        bash zsh qol utils
 ModuleLoad laptop       power
