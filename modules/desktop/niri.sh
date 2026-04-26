7e6b9ff615432070476e0dcbdd6ceafe7ce4235a
Evann DREUMONT <git@evann.dev>
2026-04-26 22:58:16 +0200

    feat(modules/desktop/niri): improve ecosystem support

--- modules/desktop/niri.sh
+++ modules/desktop/niri.sh
@@ -1,8 +1,11 @@
 AddPackage niri # A scrollable-tiling Wayland compositor
 AddPackage xdg-desktop-portal-gnome # Backend implementation for xdg-desktop-portal for the GNOME desktop environment
+AddPackage --foreign oniri # A tool that automatically maximizes the only window of a niri workspace
+AddPackage --foreign nirius # Utility commands for the niri compositor
+AddPackage --foreign niri-autoselect-portal-git # Always auto select the niri dynamic cast target without any prompts
 
-# ecosystem
-AddPackage hypridle # hyprland’s idle daemon
+# Ecosystem
+AddPackage swayidle # Idle management daemon for Wayland
 AddPackage hyprlock # hyprland’s GPU-accelerated screen locking utility
 AddPackage hyprpaper # a blazing fast wayland wallpaper utility with IPC controls
 
@@ -25,7 +28,10 @@ AddPackage swayosd # A GTK based on screen display for keyboard shortcuts like c
 AddPackage waybar # Highly customizable Wayland bar for Sway and Wlroots based compositors
 
 # Launcher
-AddPackage fuzzel # Application launcher for wlroots based Wayland compositors
+AddPackage --foreign vicinae-bin # Raycast like FOSS app on finux
 
 # Dynamic output configuration
 AddPackage kanshi # Dynamic output configuration for Wayland WMs
+
+# Cursor theme
+AddPackage --foreign bibata-cursor-theme-bin # Material Based Cursor Theme
