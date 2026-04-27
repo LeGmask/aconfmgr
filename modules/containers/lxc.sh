49ab3f2814ca7aa18857f9c0490d4975d7a30163
Evann DREUMONT <git@evann.dev>
2026-04-27 21:33:00 +0200

    feat(modules/containers/lxc): add support for rootless containers

--- modules/containers/lxc.sh
+++ modules/containers/lxc.sh
@@ -1,1 +1,19 @@
 AddPackage lxc # Linux Containers
+
+# network configuration
+AddPackage dnsmasq # Lightweight, easy to configure DNS forwarder and DHCP server
+cat > "$(CreateFile /etc/default/lxc-net)" <<EOF
+USE_LXC_BRIDGE="true"
+EOF
+
+# rootless containers
+systemd_files+=("system/user@.service.d/delegate.conf")
+cat > "$(CreateFile /etc/systemd/system/user@.service.d/delegate.conf)" <<EOF
+[Service]
+Delegate=cpu cpuset io memory pids
+EOF
+
+cat > "$(CreateFile /etc/lxc/lxc-usernet)" <<EOF
+$(id -un) veth lxcbr0 10
+EOF
+
