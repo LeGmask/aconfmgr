c47f3a0fa05a4b65473694fae68e36950746db83
Evann DREUMONT <git@evann.dev>
2026-05-26 19:38:27 +0200

    feat(modules/apps/entertainment): add ffmpeg + jellyfin-desktop

--- modules/apps/entertainment.sh
+++ modules/apps/entertainment.sh
@@ -1,2 +1,4 @@
 AddPackage spotify-launcher # Client for spotify's apt repository in Rust for Arch Linux
 AddPackage vlc # Free and open source cross-platform multimedia player and framework
+AddPackage vlc-plugin-ffmpeg # Free and open source cross-platform multimedia player and framework - FFMPEG plugins
+AddPackage --foreign jellyfin-desktop # Jellyfin Desktop Client
