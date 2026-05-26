717d4c443a611bcbe310fe3ab4a0e4eefb2cc8ec
Evann DREUMONT <git@evann.dev>
2026-05-26 19:41:23 +0200

    feat(modules/development/puppet): add puppet-editor-services and ignore pdk cache changes

--- modules/development/puppet.sh
+++ modules/development/puppet.sh
@@ -1,5 +1,8 @@
 AddPackage --foreign pdk # Puppet Development Kit bundles tools and helpers to create high quality puppet modules
+AddPackage --foreign puppet-editor-services # Language server implementation for puppet
 AddPackage --foreign puppet-lint # Check that your Puppet manifests conform to the style guide.
 AddPackage --foreign ruby-hiera-eyaml # OpenSSL Encryption backend for Hiera
 AddPackage puppet # Server automation framework and application
 AddPackage r10k # Provides a general purpose toolset for deploying Puppet environments and modules.
+
+IgnorePath /opt/puppetlabs/pdk/share/cache/\*
