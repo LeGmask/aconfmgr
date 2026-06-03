646758ecf09745ad9de756b50316fcd8da268d17
Evann DREUMONT <git@evann.dev>
2026-06-03 23:45:20 +0200

    feat(modules/development/puppet): move to openvox

--- modules/development/puppet.sh
+++ modules/development/puppet.sh
@@ -2,7 +2,7 @@ AddPackage --foreign pdk # Puppet Development Kit bundles tools and helpers to c
 AddPackage --foreign puppet-editor-services # Language server implementation for puppet
 AddPackage --foreign puppet-lint # Check that your Puppet manifests conform to the style guide.
 AddPackage --foreign ruby-hiera-eyaml # OpenSSL Encryption backend for Hiera
-AddPackage puppet # Server automation framework and application
+AddPackage openvox # Server automation framework and application
 AddPackage r10k # Provides a general purpose toolset for deploying Puppet environments and modules.
 
 IgnorePath /opt/puppetlabs/pdk/share/cache/\*
