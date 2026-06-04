046cc2ebd8f62fe7184cb0707a961b20882aa13f
Evann <evann@grifon.fr>
2026-06-04 10:18:35 +0200

    feat(modules/network/utils): add iftop + netcat + rdap + ethtool

--- modules/networks/utils.sh
+++ modules/networks/utils.sh
@@ -1,8 +1,12 @@
+AddPackage bind # A complete, highly portable implementation of the DNS protocols
+AddPackage ethtool # Utility for controlling network drivers and hardware
+AddPackage iftop # Display bandwidth usage on an interface
+AddPackage inetutils # A collection of common network programs
 AddPackage mtr # Combines the functionality of traceroute and ping into one tool (CLI version)
+AddPackage openbsd-netcat # TCP/IP swiss army knife. OpenBSD variant.
 AddPackage tcpdump # Powerful command-line packet analyzer
 AddPackage whois # Intelligent WHOIS client
 AddPackage wireguard-tools # next generation secure network tunnel - tools for configuration
-AddPackage inetutils # A collection of common network programs
-AddPackage bind # A complete, highly portable implementation of the DNS protocols
 
 AddPackage --foreign netcalc # IP network calculator - Simplified clone of sipcalc with ipcalc looks
+AddPackage --foreign openrdap-client # OpenRDAP is an command line RDAP client implementation in Go.
