1e102640dc7db1d8dbafbe641ad5040f63ffa0a7
Evann <evann@grifon.fr>
2026-06-12 10:48:28 +0200

    feat(modules/development/puppet): add jig

--- modules/development/puppet.sh
+++ modules/development/puppet.sh
@@ -4,5 +4,6 @@ AddPackage --foreign puppet-lint # Check that your Puppet manifests conform to t
 AddPackage --foreign ruby-hiera-eyaml # OpenSSL Encryption backend for Hiera
 AddPackage openvox # Server automation framework and application
 AddPackage r10k # Provides a general purpose toolset for deploying Puppet environments and modules.
+AddPackage jig # A tool for building and publishing Puppet modules
 
 IgnorePath /opt/puppetlabs/pdk/share/cache/\*
