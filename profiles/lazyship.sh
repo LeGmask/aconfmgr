762d57a29194579e7d2ec93427645bbf1022b324
Evann DREUMONT <git@evann.dev>
2026-09-18 00:47:41 +0200

    feat(profiles): add lazyship

---
+++ profiles/lazyship.sh
@@ -0,0 +1,8 @@
+ModuleLoad linux        zram
+ModuleLoad networks     networkmanager bluetooth utils
+ModuleLoad desktop      dotfiles niri
+ModuleLoad apps         entertainment keyboard mail tools
+ModuleLoad shell        bash zsh qol utils
+ModuleLoad laptop       power
+
+ModuleLoad development  ruby puppet python
