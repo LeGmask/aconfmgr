8774da68c14b332d2aca241dfba9be1e70fd947f
Evann DREUMONT <git@evann.dev>
2026-09-18 02:09:26 +0200

    fix(modules/apps/entertainment): replace jellifin with jellium

--- modules/apps/entertainment.sh
+++ modules/apps/entertainment.sh
@@ -1,4 +1,4 @@
 AddPackage spotify-launcher # Client for spotify's apt repository in Rust for Arch Linux
 AddPackage vlc # Free and open source cross-platform multimedia player and framework
 AddPackage vlc-plugin-ffmpeg # Free and open source cross-platform multimedia player and framework - FFMPEG plugins
-AddPackage --foreign jellyfin-desktop # Jellyfin Desktop Client
+AddPackage --foreign jellium-desktop-git # A Jellyfin Desktop Client
