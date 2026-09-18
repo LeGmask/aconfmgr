be5fcb337ccaa138bdbed02d6500eb7f8e1f8ec4
Evann DREUMONT <git@evann.dev>
2026-09-18 02:08:31 +0200

    fix(modules/networks/utils): netcalc no longer foreign

--- modules/networks/utils.sh
+++ modules/networks/utils.sh
@@ -3,11 +3,11 @@ AddPackage ethtool # Utility for controlling network drivers and hardware
 AddPackage iftop # Display bandwidth usage on an interface
 AddPackage inetutils # A collection of common network programs
 AddPackage mtr # Combines the functionality of traceroute and ping into one tool (CLI version)
+AddPackage netcalc # IP network calculator - Simplified clone of sipcalc with ipcalc looks
 AddPackage openbsd-netcat # TCP/IP swiss army knife. OpenBSD variant.
 AddPackage tcpdump # Powerful command-line packet analyzer
 AddPackage whois # Intelligent WHOIS client
 AddPackage wireguard-tools # next generation secure network tunnel - tools for configuration
 
-AddPackage --foreign netcalc # IP network calculator - Simplified clone of sipcalc with ipcalc looks
 AddPackage --foreign openrdap-client # OpenRDAP is an command line RDAP client implementation in Go.
 AddPackage --foreign oui # MAC Address CLI Toolkit
