ac1978c9001a37810cf62dacde8acbda9b9bf1c5
Evann DREUMONT <git@evann.dev>
2026-09-18 02:08:00 +0200

    fix(modules/dev/puppet): ruby-hiera-eyaml no longer foreign

--- modules/development/puppet.sh
+++ modules/development/puppet.sh
@@ -1,7 +1,7 @@
 AddPackage --foreign pdk # Puppet Development Kit bundles tools and helpers to create high quality puppet modules
 AddPackage --foreign puppet-editor-services # Language server implementation for puppet
 AddPackage --foreign puppet-lint # Check that your Puppet manifests conform to the style guide.
-AddPackage --foreign ruby-hiera-eyaml # OpenSSL Encryption backend for Hiera
+AddPackage ruby-hiera-eyaml # OpenSSL Encryption backend for Hiera
 AddPackage openvox # Server automation framework and application
 AddPackage r10k # Provides a general purpose toolset for deploying Puppet environments and modules.
 AddPackage jig # A tool for building and publishing Puppet modules
